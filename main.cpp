98f6dd136fe3df3ba33fec3acaaf59d84e8b3503
384379224@qq.com <384379224@qq.com>
2024-07-16 21:35:09 +0800

    use macro

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <string>
 #include <json/json.h>
 
+#define REFLECT_PP_FOREACH_2(f, x, y) f(x) f(y)
+#define REFLECT_PP_FOREACH_3(f, x, y, z) f(x) f(y) f(z)
+
 struct Student
 {
     std::string name;
@@ -11,16 +14,28 @@ struct Student
 template<class T>
 struct reflect_trait{};
 
-template<>
-struct reflect_trait<Student>
-{
-    template<class Func>
-    static void for_each_members(Student& stu, Func&& func)
-    {
-        func("name", stu.name);
-        func("age", stu.age);
-    }
+#define REFLECT_BEGIN(Type) \
+template<> \
+struct reflect_trait<Student> \
+{ \
+    template<class Func> \
+    static void for_each_members(Student& stu, Func&& func) \
+    { \
+
+#define REFLECT_PER_MEMBER(x) \
+    func(#x, stu.x);
+
+#define REFLECT_END() \
+    } \
 };
+
+
+#define REFLECT(Type, ...) \
+REFLECT_BEGIN(Student) \
+REFLECT_PP_FOREACH_2(REFLECT_PER_MEMBER, __VA_ARGS__); \
+REFLECT_END()
+
+REFLECT(Student, name, age);
 
 template<class T>
 std::string serialize(T& object)
@@ -28,7 +43,7 @@ std::string serialize(T& object)
     Json::Value root;
     // root["name"] = stu.name;
     // root["age"] = stu.age;
-    reflect_trait<T>::for_each_members(object, [&](const char* key, auto& value){
+    reflect_trait<T>::for_each_members(object, [&](const char* key, auto& value){ 
         root[key] = value;
     });
 
